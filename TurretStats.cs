1cfc0a2cc83a2e808fa7f9d87a2b5be823f7de4b
Box-Of-Hats <ja1ke@hotmail.co.uk>
2022-07-17 00:20:18 +0100

    fix font for stats list

--- TurretStats.cs
+++ TurretStats.cs
@@ -2,12 +2,15 @@ using gmtkjame2022rollthedice;
 using gmtkjame2022rollthedice.Helpers;
 using Godot;
 using System;
+using System.Linq;
 
 public class TurretStats : Control
 {
     public VBoxContainer Container { get; set; }
 
     public ITurret Turret { get; set; }
+
+    private Label _templateLabel { get; set; }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -18,11 +21,16 @@ public class TurretStats : Control
         {
             LoadTurret(Turret);
         }
+
+        //Save the first label so we can reference the theme later
+        _templateLabel = Container.GetChild<Label>(0);
+        _templateLabel.Visible = false;
+
     }
 
     public void LoadTurret(ITurret turret)
     {
-        foreach (var child in Helpers.GetChildrenOfType<Control>(Container))
+        foreach (var child in Helpers.GetChildrenOfType<Control>(Container).Skip(1))
         {
             child.QueueFree();
         }
@@ -36,11 +44,10 @@ public class TurretStats : Control
 
     public Label MakeLabel(string text, float size = 1f)
     {
-        var label = new Label
-        {
-            Text = text,
-            RectScale = new Vector2(size, size),
-        };
+        var label = _templateLabel.Duplicate() as Label;
+        label.Text = text;
+        label.Visible = true;
+
         return label;
     }
 
